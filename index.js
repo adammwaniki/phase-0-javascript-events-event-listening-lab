d10bcef3751f2ebfdf4c775d07e56b1090b14b3c
Adam Mwaniki <adamndegwa@protonmail.com>
2024-03-31 00:06:32 +0300

    Completes the event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+
 function addingEventListener() {
+    // Get the button element using its id
+    const input = document.getElementById('button');
+//event listener to listen for a click
+    input.addEventListener('click', function() {
+  alert('I was clicked!');
+    });
 }
